49dbdb62eea5fc79ff9de88799609d64ca53e6fa
glorydays <glorydays11@gmail.com>
2025-12-04 00:54:51 +0900

    プロジェクトファイルを追加。

---
+++ table-inspection/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace table_inspection
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
